e0c498219895b880a32e5f5020f40e695d980732
ya20087987 <ya20087987@wipro.com>
2019-11-26 19:09:54 +0530

    fourth commit

--- src/task1/components/CardsList.js
+++ src/task1/components/CardsList.js
@@ -4,7 +4,19 @@ import Cards from './Cards';
 export default class CardsList extends React.Component {
   render () {
     var array = this.props.cards.map(data => (
-        <Cards key={data.id} name={data.name} username={data.username} email={data.email} street={data.address.street} suite={data.address.suite} city={data.address.city} zipcode={data.address.zipcode} phone={data.phone} website={data.website} companyName={data.company.name} catchphrase={data.company.catchPhrase} bs={data.company.bs} />
+        <Cards 
+        key={data.id} 
+        name={data.name} 
+        username={data.username} 
+        email={data.email} 
+        street={data.address.street} 
+        suite={data.address.suite} 
+        city={data.address.city} 
+        zipcode={data.address.zipcode} 
+        phone={data.phone} 
+        website={data.website} 
+        companyName={data.company.name} 
+        catchphrase={data.company.catchPhrase} bs={data.company.bs} />
           
         
     ));
